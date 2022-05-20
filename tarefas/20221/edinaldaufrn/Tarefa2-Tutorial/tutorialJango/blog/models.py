bded39aa2594ebdaafc0947fa2d133560fbd858a
Edinalda Cristina <edinaldacristam@mail.com>
2022-05-20 15:58:10 -0300

    #143 criando modelo post

---
+++ tarefas/20221/edinaldaufrn/Tarefa2-Tutorial/tutorialJango/blog/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
