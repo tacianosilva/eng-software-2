371b8f4b408fa09e4ad708904d0be406956865fd
Edinalda Cristina <edinaldacristam@mail.com>
2022-05-20 16:20:30 -0300

    #143 atualizando a interface admin

--- tarefas/20221/edinaldaufrn/Tarefa2-Tutorial/tutorialJango/blog/admin.py
+++ tarefas/20221/edinaldaufrn/Tarefa2-Tutorial/tutorialJango/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "author", "created")
--- tarefas/20221/edinaldaufrn/Tarefa2-Tutorial/tutorialJango/blog/models.py
+++ tarefas/20221/edinaldaufrn/Tarefa2-Tutorial/tutorialJango/blog/models.py
@@ -10,4 +10,6 @@ class Post(models.Model):
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    
+
+    def __str__(self) -> str:
+        return self.title
