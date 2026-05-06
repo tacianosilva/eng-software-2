e9f914442a95cd520e0b63df95cae633e7327f2d
Nathan L. <nathanlpsr@gmail.com>
2026-05-05 21:17:36 -0300

    Implementa servico de inscricao para operacoes CRUD #558

---
+++ projetos/20261/nlopesr/src/inscricao_service.py
@@ -0,0 +1,22 @@
+﻿"""Simple enrollment service used to demonstrate CRUD unit tests."""
+
+
+class InscricaoService:
+    def __init__(self, repository):
+        self.repository = repository
+
+    def inserir(self, dados_inscricao):
+        """Create a new enrollment."""
+        return self.repository.create(dados_inscricao)
+
+    def atualizar(self, inscricao_id, dados_inscricao):
+        """Update an existing enrollment."""
+        return self.repository.update(inscricao_id, dados_inscricao)
+
+    def deletar(self, inscricao_id):
+        """Delete an enrollment."""
+        return self.repository.delete(inscricao_id)
+
+    def consultar(self, inscricao_id):
+        """Get an enrollment by id."""
+        return self.repository.get_by_id(inscricao_id)
